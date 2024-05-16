25e6497cb63532b658834ff8854c9eed4e86976e
Christian Meffert <christian.meffert@googlemail.com>
2024-05-16 20:41:16 +0200

    feat: Add program arguments for interval, threshold and action

--- shutdownonidle.py
+++ shutdownonidle.py
@@ -7,9 +7,12 @@ import sys
 import time
 
 
-DEFAULT_INTERVAL_SECONDS = 10
+DEFAULT_INTERVAL_SECONDS = 60
 DEFAULT_PRIORITY = 5
-DEFAULT_IDLE_THRESHOLD = 2
+DEFAULT_IDLE_THRESHOLD = 5
+
+ACTION_SHUTDOWN = "shutdown"
+ACTION_SUSPEND = "suspend"
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
@@ -44,9 +47,9 @@ def main(args: list[str] | None = None) -> int:
     state = IdleState()
 
     my_scheduler = sched.scheduler(time.time, time.sleep)
-    my_scheduler.enter(DEFAULT_INTERVAL_SECONDS, DEFAULT_PRIORITY, run, (my_scheduler, state))
+    my_scheduler.enter(opts.interval, DEFAULT_PRIORITY, run, (my_scheduler, state, opts))
 
-    logger.info(f"Scheduled first idle check to run in {DEFAULT_INTERVAL_SECONDS} seconds")
+    logger.info(f"Scheduled first idle check to run in {opts.interval} seconds")
     my_scheduler.run()
 
     return 0
@@ -59,10 +62,40 @@ def get_parser() -> argparse.ArgumentParser:
         An argparse parser.
     """
     parser = argparse.ArgumentParser(prog="shutdownonidle")
+    parser.add_argument(
+        "-i",
+        "--interval",
+        action="store",
+        type=int,
+        default=DEFAULT_INTERVAL_SECONDS,
+        metavar="INTERVAL_SECONDS",
+        dest="interval",
+        help="Specifies the time interval in seconds for checking if the system is idle. Default: %(default)s.",
+    )
+    parser.add_argument(
+        "-t",
+        "--threshold",
+        action="store",
+        type=int,
+        default=DEFAULT_IDLE_THRESHOLD,
+        metavar="IDLE_THRESHOLD",
+        dest="threshold",
+        help="Specifies number of consecutive checks after which the system is considered as idle. Default: %(default)s.",
+    )
+    parser.add_argument(
+        "-a",
+        "--action",
+        action="store",
+        default=ACTION_SHUTDOWN,
+        choices=[ACTION_SHUTDOWN, ACTION_SUSPEND],
+        metavar="ACTION",
+        dest="action",
+        help="Action to perform, when the system is considered idle. Supported values: %(choices)s. Default: %(default)s.",
+    )
     return parser
 
 
-def run(my_scheduler: sched.scheduler, state: IdleState):
+def run(my_scheduler: sched.scheduler, state: IdleState, opts: argparse.ArgumentParser):
     has_login = check_user_sessions()
     has_smbsession = check_smbsessions()
     if has_login or has_smbsession:
@@ -72,14 +105,17 @@ def run(my_scheduler: sched.scheduler, state: IdleState):
         state.increment()
         logger.info(f"No activity detected, incrementing idle counter {state.count}: active login session = {has_login}, active SMB session = {has_smbsession}")
 
-    if state.count >= DEFAULT_IDLE_THRESHOLD:
-        logger.info(f"Idle counter threshold reached {state.count} >= {DEFAULT_IDLE_THRESHOLD}: running on_idle action")
-        on_idle()
-    my_scheduler.enter(DEFAULT_INTERVAL_SECONDS, DEFAULT_PRIORITY, run, (my_scheduler, state))
+    if state.count >= opts.threshold:
+        logger.info(f"Idle counter threshold reached {state.count} >= {opts.threshold}: running on_idle action")
+        on_idle(opts.action)
+    my_scheduler.enter(opts.interval, DEFAULT_PRIORITY, run, (my_scheduler, state))
 
 
-def on_idle() -> None:
-    exec("shutdown", "now")
+def on_idle(action: str) -> None:
+    if action == ACTION_SHUTDOWN:
+        exec("shutdown", "now")
+    elif action == ACTION_SUSPEND:
+        exec("systemctl", "suspend")
 
 
 def check_user_sessions() -> bool:
